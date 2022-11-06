d0bf3c8b45c59c88bfa7c90b699b4fce110187c0
Jonatan Fernando Franco Cardenas <jffc314@gmail.com>
2022-11-06 11:45:17 -0500

    Interfaces creadas
    Correccion por merge y union erronea

--- www/js/index.js
+++ www/js/index.js
@@ -6,3 +6,15 @@ function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
 }
+
+function goPlay() {
+    window.open("./html/coordinador_jf_wr/create_or_join.html", "_self")
+}
+
+function goConfigurations() {
+    window.open("./html/coordinador_jf_wr/configurations.html", "_self")
+}
+
+function goCredits() {
+    window.open("./html/coordinador_jf_wr/credits.html", "_self")
+}
