c536300c85a4be1f54b44b47e74c74068ee887b6
SergioS11 <sergiosalas172017@hotmail.com>
2022-11-07 22:44:50 -0500

    Pruebas con acelerometro

--- www/js/game_tp_ss/game_tp_ss.js
+++ www/js/game_tp_ss/game_tp_ss.js
@@ -1,160 +1,90 @@
+document.addEventListener('deviceready', onDeviceReady, false);
 
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
 
-function handleOrientation(event) {
-  updateFieldIfNotNull('Orientation_a', event.alpha);
-  updateFieldIfNotNull('Orientation_b', event.beta);
-  updateFieldIfNotNull('Orientation_g', event.gamma);
-  incrementEventCount();
+    alert('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
 }
 
-function incrementEventCount() {
-  let counterElement = document.getElementById("num-observed-events")
-  let eventCount = parseInt(counterElement.innerHTML)
-  counterElement.innerHTML = eventCount + 1;
+var photos = new Array()
+var which = 0
+var directoryImages = "/www/img/game_tp_ss/"
+////cambia las variables necesarias
+photos[0] = "img1.png"
+photos[1] = "img2.png"
+photos[2] = "img3.png"
+photos[3] = "img4.png"
+photos[4] = "img5.png"
+photos[5] = "img6.png"
+photos[6] = "img7.png"
+
+var delay = 5000 //En milisegundos,1000=1segundo
+var count = 1;
+
+var cubeimage = new Array()
+for (i = 0; i < photos.length; i++) {
+  cubeimage[i] = new Image()
+  cubeimage[i].src = directoryImages + photos[i]
 }
 
-function updateFieldIfNotNull(fieldName, value, precision = 10) {
-  if (value != null)
-    document.getElementById(fieldName).innerHTML = value.toFixed(precision);
+function movecube() {
+  if (window.createPopup)
+    cube.filters[0].apply()
+  document.images.cube.src = cubeimage[count].src;
+  if (window.createPopup)
+    cube.filters[0].play()
+  count++;
+  if (count == cubeimage.length)
+    count = 0;
+  setTimeout("movecube()", delay)
 }
 
-function handleMotion(event) {
-  updateFieldIfNotNull('Accelerometer_gx', event.accelerationIncludingGravity.x);
-  updateFieldIfNotNull('Accelerometer_gy', event.accelerationIncludingGravity.y);
-  updateFieldIfNotNull('Accelerometer_gz', event.accelerationIncludingGravity.z);
+window.onload = new Function("setTimeout('movecube()',delay)")
 
-  updateFieldIfNotNull('Accelerometer_x', event.acceleration.x);
-  updateFieldIfNotNull('Accelerometer_y', event.acceleration.y);
-  updateFieldIfNotNull('Accelerometer_z', event.acceleration.z);
 
-  updateFieldIfNotNull('Accelerometer_i', event.interval, 2);
+//Varables para la conexión
+const mysql = require('mysql');
+const connection =  mysql.createConnection({
+  host:"ec2-52-22-136-117.compute-1.amazonaws.com",
+  database: "ddcv39k2aku9it",
+  user:"xfqtlewebtgskt",
+  port:5432,
+  password:" 6860ded4dfe1176e5fac22e92b0aa971fb8f5b6c1537ba132542d092b1bcb706",
+})
 
-  updateFieldIfNotNull('Gyroscope_z', event.rotationRate.alpha);
-  updateFieldIfNotNull('Gyroscope_x', event.rotationRate.beta);
-  updateFieldIfNotNull('Gyroscope_y', event.rotationRate.gamma);
-  incrementEventCount();
+//Conexion con la BD
+connection.connect((err)=>{
+  if(err) throw err
+  alert('La conexión funciona')
+})
+
+var score = 0;
+var score_html = document.getElementById('score')
+
+
+//Validación del juego
+function motion(event) {
+    //Verificar el movimiento
+    if(event.accelerationIncludingGravity.z>4){
+      score = score + 50;
+      score_html.innerHTML = ''+score;
+    }
+    if(score == 1800){
+      connection.query('SELECT * FROM GAMES',(err,data)=>{
+        if(err) throw err
+        alert('Los datos de la tabla Juegos son estos')
+        alert(data)
+      }
+    )
+    }
+
+}
+if (window.DeviceOrientationEvent) {
+  window.addEventListener("deviceorientation", orientation, false);
+} else {
+  console.log("DeviceOrientationEvent is not supported");
 }
 
-let is_running = false;
-let demo_button = document.getElementById("start_demo");
-demo_button.onclick = function (e) {
-  e.preventDefault();
 
-  // Request permission for iOS 13+ devices
-  if (
-    DeviceMotionEvent &&
-    typeof DeviceMotionEvent.requestPermission === "function"
-  ) {
-    DeviceMotionEvent.requestPermission();
-  }
-
-  if (is_running) {
-    window.removeEventListener("devicemotion", handleMotion);
-    window.removeEventListener("deviceorientation", handleOrientation);
-    demo_button.innerHTML = "Start demo";
-    demo_button.classList.add('btn-success');
-    demo_button.classList.remove('btn-danger');
-    is_running = false;
-  } else {
-    window.addEventListener("devicemotion", handleMotion);
-    window.addEventListener("deviceorientation", handleOrientation);
-    document.getElementById("start_demo").innerHTML = "Stop demo";
-    demo_button.classList.remove('btn-success');
-    demo_button.classList.add('btn-danger');
-    is_running = true;
-  }
-};
-
-
-
-
-// document.addEventListener('deviceready', onDeviceReady, false);
-
-// function onDeviceReady() {
-//     // Cordova is now initialized. Have fun!
-
-//     alert('Running cordova-' + cordova.platformId + '@' + cordova.version);
-//     document.getElementById('deviceready').classList.add('ready');
-// }
-
-// var photos = new Array()
-// var which = 0
-// var directoryImages = "/www/img/game_tp_ss/"
-// ////cambia las variables necesarias
-// photos[0] = "img1.png"
-// photos[1] = "img2.png"
-// photos[2] = "img3.png"
-// photos[3] = "img4.png"
-// photos[4] = "img5.png"
-// photos[5] = "img6.png"
-// photos[6] = "img7.png"
-
-// var delay = 5000 //En milisegundos,1000=1segundo
-// var count = 1;
-
-// var cubeimage = new Array()
-// for (i = 0; i < photos.length; i++) {
-//   cubeimage[i] = new Image()
-//   cubeimage[i].src = directoryImages + photos[i]
-// }
-
-// function movecube() {
-//   if (window.createPopup)
-//     cube.filters[0].apply()
-//   document.images.cube.src = cubeimage[count].src;
-//   if (window.createPopup)
-//     cube.filters[0].play()
-//   count++;
-//   if (count == cubeimage.length)
-//     count = 0;
-//   setTimeout("movecube()", delay)
-// }
-
-// window.onload = new Function("setTimeout('movecube()',delay)")
-
-
-// //Varables para la conexión
-// const mysql = require('mysql');
-// const connection =  mysql.createConnection({
-//   host:"ec2-52-22-136-117.compute-1.amazonaws.com",
-//   database: "ddcv39k2aku9it",
-//   user:"xfqtlewebtgskt",
-//   port:5432,
-//   password:" 6860ded4dfe1176e5fac22e92b0aa971fb8f5b6c1537ba132542d092b1bcb706",
-// })
-
-// //Conexion con la BD
-// connection.connect((err)=>{
-//   if(err) throw err
-//   alert('La conexión funciona')
-// })
-
-// var score = 0;
-// var score_html = document.getElementById('score')
-
-
-// //Validación del juego
-// function motion(event) {
-//     //Verificar el movimiento
-//     if(event.accelerationIncludingGravity.z>8){
-//       score = score + 50;
-//       score_html.innerHTML = ''+score;
-//     }
-//     if(score == 1800){
-//       connection.query('SELECT * FROM GAMES',(err,data)=>{
-//         if(err) throw err
-//         alert('Los datos de la tabla Juegos son estos')
-//         alert(data)
-//       }
-//     )
-//     }
-
-// }
-// if (window.DeviceOrientationEvent) {
-//   window.addEventListener("deviceorientation", orientation, false);
-// } else {
-//   console.log("DeviceOrientationEvent is not supported");
-// }
-
-
-// connection.end()
+connection.end()
