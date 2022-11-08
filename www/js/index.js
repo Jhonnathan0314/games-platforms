178b38b6ed9439bfce63eab3d330002d6adeb2ec
Jonatan Fernando Franco Cardenas <jffc314@gmail.com>
2022-11-08 10:39:02 -0500

    Funcionalidad configuracion
    Vibracion app

--- www/js/index.js
+++ www/js/index.js
@@ -1,20 +1,6 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
-}
-
-function goPlay() {
-    window.open("./html/coordinador_jf_wr/create_or_join.html", "_self")
-}
-
-function goConfigurations() {
-    window.open("./html/coordinador_jf_wr/configurations.html", "_self")
-}
-
-function goCredits() {
-    window.open("./html/coordinador_jf_wr/credits.html", "_self")
 }
