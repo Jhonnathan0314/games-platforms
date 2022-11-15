c42375aeb1db987d4368f8da8cf2e6fb91a7faf7
Tpoveda02 <75458574+Tpoveda02@users.noreply.github.com>
2022-11-15 00:15:22 -0500

    TimeStart

--- www/js/game_tp_ss/level_one_tp_ss.js
+++ www/js/game_tp_ss/level_one_tp_ss.js
@@ -1,26 +1,31 @@
-
+window.onload = countTime;
 //Declaración de variables
 var score = 0;
 var score_html = document.getElementById('score')
-var time =  10;
+var time = 16;
 
-//Detectar el movimiento del dispositivo
-setTimeout(
-  window.addEventListener("devicemotion", function(event){
-  //Metodo - aparecer imagen explicativa
-  document.getElementById('img').setAttribute("hidden", "hidden")
-  //Obtener la aceleración en el X 
-  accelerometer = (event.acceleration.x).toFixed(10)
-  if(accelerometer >= 65){
-    score = score + 4;
-    score_html.textContent = "PUNTAJE:0" + score
+function countTime() {
+  if (time == 10) {
+    //Detectar el movimiento del dispositivo
+    window.addEventListener("devicemotion", function (event) {
+      //Metodo - aparecer imagen explicativa
+      document.getElementById('img').setAttribute("hidden", "hidden")
+      //Obtener la aceleración en el X 
+      accelerometer = (event.acceleration.x).toFixed(10)
+      if (accelerometer >= 65) {
+        score = score + 4;
+        score_html.textContent = "PUNTAJE:0" + score
+      }
+      if (score == 400) {
+        sessionStorage.setItem("score", 400)
+        this.window.open("../../html/game_tp_ss/level_two_tp_ss.html", "_self");
+      }
+    }, true);
+  } else {
+    time -= 1;
+    setTimeout("countTime()", 1000);
   }
-  if(score == 400){
-    sessionStorage.setItem("score",400)
-    this.window.open("../../html/game_tp_ss/level_two_tp_ss.html","_self");
-  }
-}, true),3000);
-
+}
 
 
 
--- www/js/game_tp_ss/level_two_tp_ss.js
+++ www/js/game_tp_ss/level_two_tp_ss.js
@@ -1,17 +1,32 @@
+window.onload = countTime;
+//Declaración de variables
 var score = 0;
 var score_html = document.getElementById('score')
+var time = 26;
 
-
-window.addEventListener("devicemotion", function(event){
-  accelerometer = (event.acceleration.z).toFixed(10)
-  document.getElementById("Accelerometer_gz").innerHTML = event.acceleration.z.toFixed(10);
-  if(accelerometer >= 65){
-    score = score + 2;
-    score_html.textContent = "PUNTAJE:0" + score
+function countTime() {
+  if (time == 20) {
+    //Detectar el movimiento del dispositivo
+    window.addEventListener("devicemotion", function (event) {
+      //Metodo - aparecer imagen explicativa
+      document.getElementById('img').setAttribute("hidden", "hidden")
+      //Obtener la aceleración en el X 
+      accelerometer = (event.acceleration.y).toFixed(10)
+      if (accelerometer >= 65) {
+        score = score + 4;
+        score_html.textContent = "PUNTAJE:0" + score
+      }
+      if (score == 400) {
+        sessionStorage.setItem("score", 400)
+        this.window.open("../../html/game_tp_ss/level_two_tp_ss.html", "_self");
+      }
+    }, true);
+  } else {
+    time -= 1;
+    setTimeout("countTime()", 1000);
   }
-}, true);
+}
 
-//Metodo - aparecer imagen explicativa
-setTimeout(function () {
-  $("#img").fadeOut(1800)
-})
+
+
+
