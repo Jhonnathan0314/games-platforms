4f91285bd12acfe75b0f78ae3eda17035362e388
Tpoveda02 <75458574+Tpoveda02@users.noreply.github.com>
2022-11-15 03:38:37 -0500

    ImageGif

--- www/js/game_tp_ss/level_three_tp_ss.js
+++ www/js/game_tp_ss/level_three_tp_ss.js
@@ -2,15 +2,18 @@ window.onload = countTime;
 //Declaración de variables
 var score = 0;
 var score_html = document.getElementById('score')
-var time = 21;
+var time = 26;
+var time2 = 20;
 
 function countTime() {
-  if (time == 15) {
+  if (time == 20) {
     //Detectar el movimiento del dispositivo
+    countTime2()
     window.addEventListener("devicemotion", function (event) {
       //Metodo - aparecer imagen explicativa
+      document.getElementById('information_level').setAttribute("hidden", "hidden")
       document.getElementById('img').setAttribute("hidden", "hidden")
-      //Obtener la aceleración en el X 
+      //Obtener la aceleración en el X
       accelerometer = (event.acceleration.z).toFixed(10)
       if (accelerometer >= 65) {
         score = score + 2;
@@ -18,7 +21,7 @@ function countTime() {
       }
       if (score == 400) {
         sessionStorage.setItem("score", 400)
-        this.window.open("../../html/game_tp_ss/level_two_tp_ss.html", "_self");
+        this.window.open("../../html/game_tp_ss/finish_tp_ss.html", "_self");
       }
     }, true);
   } else {
@@ -27,6 +30,11 @@ function countTime() {
   }
 }
 
-
-
-
+function countTime2() {
+  if (time2 == 0) {
+    this.window.open("../../html/game_tp_ss/finish_tp_ss.html", "_self");
+  } else {
+    time2 -= 1;
+    setTimeout("countTime2()", 1000);
+  }
+}
