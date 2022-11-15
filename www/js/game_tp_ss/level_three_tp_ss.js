ac95b71149e5c73acc8319c1c554ee41182609fe
SergioS11 <sergiosalas172017@hotmail.com>
2022-11-14 21:22:03 -0500

    Creación de nib¿veles

--- www/js/game_tp_ss/level_three_tp_ss.js
+++ www/js/game_tp_ss/level_three_tp_ss.js
@@ -5,8 +5,9 @@ var score_html = document.getElementById('score')
 window.addEventListener("devicemotion", function(event){
   accelerometer = (event.acceleration.z).toFixed(10)
   document.getElementById("Accelerometer_gz").innerHTML = event.acceleration.z.toFixed(10);
-  if(accelerometer >= 20){
-    score = score + 50;
+  if(accelerometer >= 65){
+    score = score + 2;
     score_html.textContent = score
   }
 }, true);
+
---
+++ www/js/game_tp_ss/level_two_tp_ss.js
@@ -0,0 +1,13 @@
+var score = 0;
+var score_html = document.getElementById('score')
+
+
+window.addEventListener("devicemotion", function(event){
+  accelerometer = (event.acceleration.z).toFixed(10)
+  document.getElementById("Accelerometer_gz").innerHTML = event.acceleration.z.toFixed(10);
+  if(accelerometer >= 65){
+    score = score + 2;
+    score_html.textContent = score
+  }
+}, true);
+
